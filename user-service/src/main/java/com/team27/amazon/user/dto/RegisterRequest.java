9626906b0b149dcd21bd9bebf683d633949d4a1d
AmrAhmedNegm <amrahmednegm153@gmail.com>
2026-04-30 22:31:57 +0300

    feat(user-service): implement S1-F10 user registration (55-25195)

---
+++ user-service/src/main/java/com/team27/amazon/user/dto/RegisterRequest.java
@@ -0,0 +1,55 @@
+package com.team27.amazon.user.dto;
+
+import java.util.Map;
+
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private Map<String, Object> preferences;
+
+    public RegisterRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Map<String, Object> getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Map<String, Object> preferences) {
+        this.preferences = preferences;
+    }
+}
