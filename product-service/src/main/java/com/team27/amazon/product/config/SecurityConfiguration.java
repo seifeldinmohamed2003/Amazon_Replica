ff346e2e08e5a19b4f589a954cda2449eae1b5e4
Yahia Elghoniemy <yahiaamr90@gmail.com>
2026-05-02 19:45:04 +0300

    fix(product-service): fix securityConfig syn err (55-23312)

--- product-service/src/main/java/com/team27/amazon/product/config/SecurityConfiguration.java
+++ product-service/src/main/java/com/team27/amazon/product/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticat
                     .requestMatchers("/api/products/health").permitAll()
                     .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
                     .requestMatchers("/api/products/search/full-text").hasAnyRole("USER", "ADMIN")
-                    .anyRequest().authenticated())
+                    .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
