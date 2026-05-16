4a8f29906fd30c24311977ccdddadabb8408bf42
Omar Walid <omar.walid351@yahoo.com>
2026-05-16 13:08:10 +0300

    feat(product): add OrderServiceClient and UserServiceClient Feign interfaces (55-25394)

--- order-service/src/main/java/com/team27/amazon/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/team27/amazon/order/OrderServiceApplication.java
@@ -3,7 +3,10 @@ package com.team27.amazon.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.team27.amazon.contracts.feign")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
